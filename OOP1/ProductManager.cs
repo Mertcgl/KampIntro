0c4a09e6eee716d09d6023b5dafdc74fb3335fe9
Mert Gül <mertcangul2014@gmail.com>
2022-12-01 14:12:17 +0300

    1 Aralık Ders Sonu

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    // CRUD Create Read Update Delete operations
+    internal class ProductManager
+    {
+        //encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi.");
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " guncellendi.");
+        }
+    }
+}
