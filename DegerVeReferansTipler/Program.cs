a79591934cbe526e4b28a53fb50f1aa7521f7d4c
Mert Gül <mertcangul2014@gmail.com>
2022-12-03 13:07:30 +0300

    29 kasım ders sonu

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -15,11 +15,11 @@ namespace DegerVeReferansTipler
             int sayi1 = 10;
             int sayi2 = 30;
 
-            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar1 = new int[] { 10, 200, 30 };
             int[] sayilar2 = new int[] { 100, 200, 300 };
 
             sayilar1[0] = sayilar2[0];
-            sayilar2[0] = 999;
+            sayilar2[0] = 9990;
             Console.WriteLine(sayilar1[0]);
             Console.ReadLine();
         }
